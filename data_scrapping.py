4b512cf06e1deb7a4d03e4b9720fe5306a75065f
Naufal Rizqullah <84897477+NaufalRizqullah23@users.noreply.github.com>
2024-02-19 15:05:45 +0700

    first commit

---
+++ data_scrapping.py
@@ -0,0 +1,34 @@
+import asyncio
+from twscrape import API
+import csv
+
+tw_api = API()
+
+async def add_and_log_into_account():
+    await tw_api.pool.add_account("Sinopla23","Sandalbutut23.","dajoka23@gmail.com","Sandalbutut23.")
+    await tw_api.pool.login_all()
+
+query = '"Anti-Pancasila" until:2023-10-21 since:2023-07-21'
+scrapped_tweets = []
+
+async def fetch_tweets(query):
+    await add_and_log_into_account()
+    async for tweet in tw_api.search(query):
+        tweet_data = {
+            "Date": tweet.date,
+            "Username": tweet.user.username,
+            "Tweet": tweet.rawContent,
+        }
+        scrapped_tweets.append(tweet_data)
+
+asyncio.run(fetch_tweets(query))
+
+csv_file = "Anti-Pancasila.csv"
+with open(csv_file,"w", newline="", encoding="utf-8") as csvfile:
+    fieldnames = ["Date","Username","Tweet"]
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    for tweet_data in scrapped_tweets:
+        writer.writerow(tweet_data)
+
+print("Scraped data finished")
