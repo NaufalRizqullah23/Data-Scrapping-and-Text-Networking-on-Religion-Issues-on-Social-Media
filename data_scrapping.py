b62b5fc0523e7edc8b4937be03beb5c17cb34969
Naufal Rizqullah <84897477+NaufalRizqullah23@users.noreply.github.com>
2024-02-19 15:15:14 +0700

    Removed Personal Login Info

--- data_scrapping.py
+++ data_scrapping.py
@@ -5,7 +5,7 @@ import csv
 tw_api = API()
 
 async def add_and_log_into_account():
-    await tw_api.pool.add_account("Sinopla23","Sandalbutut23.","dajoka23@gmail.com","Sandalbutut23.")
+    await tw_api.pool.add_account("TW_Username","TW_Password.","TW_Email","Email_Password")
     await tw_api.pool.login_all()
 
 query = '"Anti-Pancasila" until:2023-10-21 since:2023-07-21'
